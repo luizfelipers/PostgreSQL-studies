a28e83d8cbe4dbc64d692cb8529ee972f933d259
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-03-04 14:18:41 -0300

    Logical operators (<, >, <=, >=)

--- Fundamentals of SQL/selectData.sql
+++ Fundamentals of SQL/selectData.sql
@@ -33,4 +33,33 @@
 	
 	-- select data from the MOVIES table that matches at least one(OR) conditional clauses
 	SELECT * FROM movies WHERE age_certificate='15' OR movie_lang='Chinese';
+	-- selecting data from the Movies table that
+	SELECT * FROM movies WHERE age_certificate='15' AND movie_lang = 'English' AND director_id=27;
+	
+	-- LOGICAL OPERATORS
+	-- < LESS THAN / MENOR QUE
+	-- <= LESS OR EQUAL THAN / MENOR OU IGUAL A
+	-- > GREATER THAN / MAIOR QUE
+	-- >= GREATER OR EQUAL THAN / MAIOR OU IGUAL A
+	
+	-- SYNTAX:
+	-- SELECT columns FROM table WHERE columnname > value      (substituir o operador lógico por qualquer um)
+	
+	-- selecting data from the movies table, in relation to their lengths
+	SELECT movie_name, movie_length FROM movies WHERE movie_length > 120;
+	SELECT movie_name, movie_length FROM movies WHERE movie_length < 120;
+	SELECT movie_name, movie_length FROM movies WHERE movie_length >= 120;
+	SELECT movie_name, movie_length FROM movies WHERE movie_length <= 120;
+	SELECT movie_name, movie_length FROM movies WHERE movie_length = 120;
+	
+	-- selecting data from the movies table, regarding it's release date
+	SELECT movie_name, release_date FROM movies WHERE release_date > '1990-01-01';
+	SELECT movie_name, release_date FROM movies WHERE release_date < '1990-01-01';
+	
+	-- selecting data from movies table regarding it's movie language. Comparison happens through strings alphabetical order
+	SELECT movie_name, movie_lang FROM movies WHERE movie_lang < 'English';
+	SELECT movie_name, movie_lang FROM movies WHERE movie_lang > 'English';
+	SELECT movie_name, movie_lang FROM movies WHERE movie_lang >= 'English';
+	SELECT movie_name, movie_lang FROM movies WHERE movie_lang <= 'English';
+	
 	

11ebdbb45a9754e02058eea87298777022a98a15
Luiz Felipe Rosa da Silveira <luizfelipers19@gmail.com>
2021-03-12 21:00:35 -0300

    Create stringFunctions.sql

---
+++ stringFunctions.sql
@@ -0,0 +1,21 @@
+-- for this file, a copy/clone of the movie_data database was created.
+
+-- UPPER and LOWER functions
+
+
+-- UPPER
+ /* 
+ SELECT UPPER('string'); -- recebe uma string como parâmetro, e retorna a mesma string em letras maiúsculas
+ 
+ SELECT UPPER(column_name) FROM table; -- recebe coluna onde o valor do registro é uma string. e retorna a mesma string em letras maiúsculas
+ */
+ 
+-- LOWER
+
+/*
+SELECT LOWER('STRING'); -- recebe uma string como parâmetro e retorna a string inteirmanete em letras minúsculas
+SELECT LOWER(column_name) FROM table;
+
+*/
+ 
+
